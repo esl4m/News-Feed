a501a2001f92cee213873ded155fbd3c60d6e8ea
Eslam Elgamal <me@esl4m.com>
2020-02-03 23:21:07 +0300

    Adds MultiSelectField to models

--- news_feed/models.py
+++ news_feed/models.py
@@ -1,12 +1,40 @@
 from django.db import models
 from datetime import datetime
+from multiselectfield import MultiSelectField
+
+
+class Article(models.Model):
+    article_name = models.CharField(max_length=50)
+    content = models.CharField(max_length=256)
+    author_name = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    def __str__(self):
+        return self.article_name
 
 
 class News(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=256)
-    # amount = models.FloatField(null=False, blank=False, default=None)
-    # period = models.DecimalField(max_digits=5, decimal_places=2)
+    article_id = models.ForeignKey('Article', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True, blank=False)
 
     def __str__(self):
         return self.name
+
+MY_RATES = (
+    (1, 'Fair'),
+    (2, 'Not Bad'),
+    (3, 'Good'),
+    (4, 'Very Good'),
+    (5, 'Excellent'))
+
+
+class Rate(models.Model):
+    rate_title = models.CharField(max_length=50)
+    article_id = models.ForeignKey('Article', on_delete=models.CASCADE)
+    rate = MultiSelectField(choices=MY_RATES, default='Fair')
+    date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    def __str__(self):
+        return self.rate_title
